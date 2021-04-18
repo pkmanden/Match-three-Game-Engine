8cbde659cbd88c61d85093b945d76824d5eefa9b
Prathisha Kunnumbrath Manden <pkmanden@mail.uni-paderborn.de>
2021-04-18 21:36:20 +0000

    Delete trial.py

--- match_3_updated/m3/trial.py
+++
@@ -1,9 +0,0 @@
-import gym
-import m3_gym_env
-env = m3_gym_env.MatchThreeEnv(100)
-# env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
-env.close()
